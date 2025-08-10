7f436a64096785b19c739a7fd46a06640aebae93
Vitor Lima <viitorlima2000@gmail.com>
2025-08-10 10:47:30 -0300

    fix: correct variable name in baleBag creation function for clarity

--- backend_bellasim/src/services/baleBagService.ts
+++ backend_bellasim/src/services/baleBagService.ts
@@ -3,12 +3,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const create = async (description: string, cost: string) => {
-  const existingBaleBag = await prisma.baleBag.create({
+  const baleBag = await prisma.baleBag.create({
     data: {
       description: description,
       cost: cost,
     },
   });
 
-  return existingBaleBag;
+  return baleBag;
 };
