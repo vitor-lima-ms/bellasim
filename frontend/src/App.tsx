fef25324bd93d89a8e5bcf663097abaa599e001f
Vitor Lima <viitorlima2000@gmail.com>
2025-08-10 00:04:30 -0300

    fix: change Container to fluid in AppContent for better layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function AppContent() {
       {auth.isAuthenticated && (
         <>
           <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
+            <Container fluid>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
