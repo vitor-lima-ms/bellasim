29a5fad4cf081055355a747d2669708c8b912a5f
Vitor Lima <viitorlima2000@gmail.com>
2025-08-10 23:53:29 -0300

    fix: correct model reference from tax to sT in create function

--- backend_bellasim/src/services/stService.ts
+++ backend_bellasim/src/services/stService.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const create = async (description: string, percent: string) => {
-  const st = await prisma.tax.create({
+  const st = await prisma.sT.create({
     data: {
       description: description,
       percent: percent,
