05acfd30a4ae9e78880c8de2d7c5bb30ed1be75d
Vitor Lima <viitorlima2000@gmail.com>
2025-08-11 12:43:09 -0300

    refactor: reseting database

--- backend_bellasim/prisma/migrations/20250810032415_create_cost_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Cost" (
-    "id" SERIAL NOT NULL,
-    "rawMaterial" TEXT NOT NULL,
-    "unit" TEXT NOT NULL,
-    "unitCost" TEXT NOT NULL,
-
-    CONSTRAINT "Cost_pkey" PRIMARY KEY ("id")
-);
