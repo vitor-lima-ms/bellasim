9fb22903567b070c8416a1e8d24fc8947be936c5
Vitor Lima <viitorlima2000@gmail.com>
2025-08-12 00:42:57 -0300

    refactor: implement deleteById method in multiple controllers for improved data management

--- backend_bellasim/src/controllers/taxController.ts
+++ backend_bellasim/src/controllers/taxController.ts
@@ -23,3 +23,15 @@ export const read = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const deleteById = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+
+  try {
+    await taxService.deleteById(id);
+
+    res.status(200).json({ message: "Deletado com sucesso!" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
