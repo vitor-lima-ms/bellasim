afb0055ff5d265d94e4764e9d46f935ccc60512e
Vitor Lima <viitorlima2000@gmail.com>
2025-08-13 14:15:23 -0300

    refactor: update Diaper component layout and improve form structure

--- frontend/src/components/Diaper.tsx
+++ frontend/src/components/Diaper.tsx
@@ -201,196 +201,210 @@ export function Diaper(props: { modelProp: string }) {
   return (
     <Card>
       <Card.Header as="h3" className="text-center">
-        Bellafral
+        {model}
       </Card.Header>
-      <Card.Body>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Row>
-            <Col md={6}>
-              <Form.Group className="mb-3" controlId="formDiaperModel">
-                <Form.Label>Modelo</Form.Label>
-                <Form.Control type="text" value={model} readOnly required />
-              </Form.Group>
-            </Col>
-            <Col md={6}>
-              <Form.Group className="mb-3" controlId="formPackageQuantity">
-                <Form.Label>Quantidade por pacote</Form.Label>
-                <Form.Control
-                  type="number"
-                  min={0}
-                  value={packageQuantity}
-                  onChange={(event) => setPackageQuantity(event.target.value)}
-                  required
-                />
-              </Form.Group>
-            </Col>
-          </Row>
+      <Row>
+        <Col md={7}>
+          <Card.Body>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Row>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formDiaperModel">
+                    <Form.Label>Modelo</Form.Label>
+                    <Form.Control type="text" value={model} readOnly required />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formPackageQuantity">
+                    <Form.Label>Quantidade por pacote</Form.Label>
+                    <Form.Control
+                      type="number"
+                      min={0}
+                      value={packageQuantity}
+                      onChange={(event) =>
+                        setPackageQuantity(event.target.value)
+                      }
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
 
-          <Form.Group className="mb-3" controlId="formRawMaterials">
-            <Form.Label>Matérias-primas</Form.Label>
-            <Card body className="text-center">
-              <p className="mb-0">
-                Aqui entrará um componente mais complexo para selecionar
-                múltiplas matérias-primas e seus respectivos pesos.
-              </p>
-            </Card>
-          </Form.Group>
+              <Form.Group className="mb-3" controlId="formRawMaterials">
+                <Form.Label>Matérias-primas</Form.Label>
+                <Card body className="text-center">
+                  <p className="mb-0">
+                    Aqui entrará um componente mais complexo para selecionar
+                    múltiplas matérias-primas e seus respectivos pesos.
+                  </p>
+                </Card>
+              </Form.Group>
 
-          <Row>
-            <Col md={6}>
-              <Form.Group className="mb-3" controlId="formPackaging">
-                <Form.Label>Embalagem (R$)</Form.Label>
-                <Form.Select
-                  value={packagingCost}
-                  onChange={(event) => setPackagingCost(event.target.value)}
-                  required
-                >
-                  <option value="" disabled>
-                    ...
-                  </option>
-                  {packagings.map((packaging) => (
-                    <option key={packaging.id} value={packaging.cost}>
-                      {packaging.cost}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col md={6}>
-              <Form.Group className="mb-3" controlId="formBaleBag">
-                <Form.Label>Saco para fardos (R$)</Form.Label>
-                <Form.Select
-                  value={baleBagCost}
-                  onChange={(event) => setBaleBagCost(event.target.value)}
-                  required
-                >
-                  <option value="" disabled>
-                    ...
-                  </option>
-                  {baleBags.map((baleBag) => (
-                    <option key={baleBag.id} value={baleBag.cost}>
-                      {baleBag.cost}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md={6}>
-              <Form.Group className="mb-3" controlId="formCommission">
-                <Form.Label>Comissão (%)</Form.Label>
-                <Form.Select
-                  value={commissionPercent}
-                  onChange={(event) => setCommissionPercent(event.target.value)}
-                  required
-                >
-                  <option value="" disabled>
-                    ...
-                  </option>
-                  {commissions.map((commission) => (
-                    <option key={commission.id} value={commission.percent}>
-                      {commission.percent}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col md={6}>
-              <Form.Group className="mb-3" controlId="formTaxes">
-                <Form.Label>Impostos (%)</Form.Label>
-                <Form.Select
-                  value={taxPercent}
-                  onChange={(event) => setTaxPercent(event.target.value)}
-                  required
-                >
-                  <option value="" disabled>
-                    ...
-                  </option>
-                  {taxes.map((tax) => (
-                    <option key={tax.id} value={tax.percent}>
-                      {tax.percent}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md={4}>
-              <Form.Group className="mb-3" controlId="formFreight">
-                <Form.Label>Frete (%)</Form.Label>
-                <Form.Select
-                  value={freightPercent}
-                  onChange={(event) => setFreightPercent(event.target.value)}
-                  required
-                >
-                  <option value="" disabled>
-                    ...
-                  </option>
-                  {freights.map((freight) => (
-                    <option key={freight.id} value={freight.percent}>
-                      {freight.percent}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col md={4}>
-              <Form.Group className="mb-3" controlId="formContributionMargin">
-                <Form.Label>Margem de Contribuição (%)</Form.Label>
-                <Form.Select
-                  value={contributionMarginPercent}
-                  onChange={(event) =>
-                    setContributionMarginPercent(event.target.value)
-                  }
-                  required
-                >
-                  <option value="" disabled>
-                    ...
-                  </option>
-                  {contributionMargins.map((contributionMargin) => (
-                    <option
-                      key={contributionMargin.id}
-                      value={contributionMargin.percent}
+              <Row>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formPackaging">
+                    <Form.Label>Embalagem (R$)</Form.Label>
+                    <Form.Select
+                      value={packagingCost}
+                      onChange={(event) => setPackagingCost(event.target.value)}
+                      required
                     >
-                      {contributionMargin.percent}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col md={4}>
-              <Form.Group className="mb-3" controlId="formST">
-                <Form.Label>Substituição Tributária (%)</Form.Label>
-                <Form.Select
-                  value={stPercent}
-                  onChange={(event) => setStPercent(event.target.value)}
-                  required
-                >
-                  <option value="" disabled>
-                    ...
-                  </option>
-                  {sts.map((sts) => (
-                    <option key={sts.id} value={sts.percent}>
-                      {sts.percent}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-          </Row>
+                      <option value="" disabled>
+                        ...
+                      </option>
+                      {packagings.map((packaging) => (
+                        <option key={packaging.id} value={packaging.cost}>
+                          {packaging.cost}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formBaleBag">
+                    <Form.Label>Saco para fardos (R$)</Form.Label>
+                    <Form.Select
+                      value={baleBagCost}
+                      onChange={(event) => setBaleBagCost(event.target.value)}
+                      required
+                    >
+                      <option value="" disabled>
+                        ...
+                      </option>
+                      {baleBags.map((baleBag) => (
+                        <option key={baleBag.id} value={baleBag.cost}>
+                          {baleBag.cost}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+              </Row>
 
-          <div className="d-grid mt-3">
-            <Button className="btn-custom-orange" type="submit">
-              Cadastrar
-            </Button>
-          </div>
-        </Form>
-      </Card.Body>
+              <Row>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formCommission">
+                    <Form.Label>Comissão (%)</Form.Label>
+                    <Form.Select
+                      value={commissionPercent}
+                      onChange={(event) =>
+                        setCommissionPercent(event.target.value)
+                      }
+                      required
+                    >
+                      <option value="" disabled>
+                        ...
+                      </option>
+                      {commissions.map((commission) => (
+                        <option key={commission.id} value={commission.percent}>
+                          {commission.percent}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formTaxes">
+                    <Form.Label>Impostos (%)</Form.Label>
+                    <Form.Select
+                      value={taxPercent}
+                      onChange={(event) => setTaxPercent(event.target.value)}
+                      required
+                    >
+                      <option value="" disabled>
+                        ...
+                      </option>
+                      {taxes.map((tax) => (
+                        <option key={tax.id} value={tax.percent}>
+                          {tax.percent}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col md={4}>
+                  <Form.Group className="mb-3" controlId="formFreight">
+                    <Form.Label>Frete (%)</Form.Label>
+                    <Form.Select
+                      value={freightPercent}
+                      onChange={(event) =>
+                        setFreightPercent(event.target.value)
+                      }
+                      required
+                    >
+                      <option value="" disabled>
+                        ...
+                      </option>
+                      {freights.map((freight) => (
+                        <option key={freight.id} value={freight.percent}>
+                          {freight.percent}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col md={4}>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="formContributionMargin"
+                  >
+                    <Form.Label>Margem de Contribuição (%)</Form.Label>
+                    <Form.Select
+                      value={contributionMarginPercent}
+                      onChange={(event) =>
+                        setContributionMarginPercent(event.target.value)
+                      }
+                      required
+                    >
+                      <option value="" disabled>
+                        ...
+                      </option>
+                      {contributionMargins.map((contributionMargin) => (
+                        <option
+                          key={contributionMargin.id}
+                          value={contributionMargin.percent}
+                        >
+                          {contributionMargin.percent}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col md={4}>
+                  <Form.Group className="mb-3" controlId="formST">
+                    <Form.Label>Substituição Tributária (%)</Form.Label>
+                    <Form.Select
+                      value={stPercent}
+                      onChange={(event) => setStPercent(event.target.value)}
+                      required
+                    >
+                      <option value="" disabled>
+                        ...
+                      </option>
+                      {sts.map((sts) => (
+                        <option key={sts.id} value={sts.percent}>
+                          {sts.percent}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <div className="d-grid mt-3">
+                <Button className="btn-custom-orange" type="submit">
+                  Cadastrar
+                </Button>
+              </div>
+            </Form>
+          </Card.Body>
+        </Col>
+        <Col md={5}></Col>
+      </Row>
     </Card>
   );
 }
