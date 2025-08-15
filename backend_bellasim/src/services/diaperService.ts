f8755893c7649ef076ad1d6ad1d6ded76c62f769
Vitor Lima <viitorlima2000@gmail.com>
2025-08-15 17:49:36 -0300

    refactor: implement createOrUpdate method in DiaperService to handle diaper creation and updates

--- backend_bellasim/src/services/diaperService.ts
+++ backend_bellasim/src/services/diaperService.ts
@@ -1,3 +1,4 @@
+import { IDiaper } from "../types/diaper";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -5,7 +6,46 @@ const prisma = new PrismaClient();
 export class DiaperService {
   private constructor() {}
 
-  static async createOrUpdate() {
-    // Hello World!
+  static async createOrUpdate(diaper: IDiaper) {
+    const updatedOrCreatedDiaper = await prisma.diaper.upsert({
+      create: {
+        model: diaper.model,
+        rawMaterials: diaper.rawMaterials,
+        rawMaterialsWeight: diaper.rawMaterialsWeight,
+        costPerRawMaterial: diaper.costPerRawMaterial,
+        packageQuantity: diaper.packageQuantity,
+        packagingCost: diaper.packagingCost,
+        baleBagCost: diaper.baleBagCost,
+        commissionPercent: diaper.commissionPercent,
+        taxesPercent: diaper.taxesPercent,
+        freightPercent: diaper.freightPercent,
+        contributionMarginPercent: diaper.contributionMarginPercent,
+        STPercent: diaper.STPercent,
+      },
+      update: {
+        rawMaterials: diaper.rawMaterials,
+        rawMaterialsWeight: diaper.rawMaterialsWeight,
+        costPerRawMaterial: diaper.costPerRawMaterial,
+        unitCost: diaper.unitCost,
+        packageQuantity: diaper.packageQuantity,
+        packagingCost: diaper.packagingCost,
+        baleBagCost: diaper.baleBagCost,
+        diaperPackageCost: diaper.diaperPackageCost,
+        diaperUnitCost: diaper.diaperUnitCost,
+        commissionPercent: diaper.commissionPercent,
+        taxesPercent: diaper.taxesPercent,
+        freightPercent: diaper.freightPercent,
+        contributionMarginPercent: diaper.contributionMarginPercent,
+        STPercent: diaper.STPercent,
+        salePrice: diaper.salePrice,
+        unitSalePrice: diaper.unitSalePrice,
+        salePriceWithST: diaper.salePriceWithST,
+        unitSalePriceWithST: diaper.unitSalePriceWithST,
+        finalSalePrice: diaper.finalSalePrice,
+      },
+      where: {
+        model: diaper.model,
+      },
+    });
   }
 }
