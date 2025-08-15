344e6c48a9d899f9a6599e2564b070e7427d5c63
Vitor Lima <viitorlima2000@gmail.com>
2025-08-15 13:51:46 -0300

    refactor: add DiaperService class for managing diaper-related operations

---
+++ backend_bellasim/src/services/diaperService.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export class DiaperService {
+  private constructor() {}
+
+  static async createOrUpdate() {
+    
+  }
+}
